e15456eef3abf1fadbca46090be886b472bafab6
Bass-Dev2 <goopyvirus@gmail.com>
2023-12-02 16:29:43 -0800

    auto working now
    i did it, WOOOOOOOO

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotAuto.java
@@ -34,6 +34,13 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.robotcore.external.tfod.Recognition;
+import org.firstinspires.ftc.vision.VisionPortal;
+import org.firstinspires.ftc.vision.tfod.TfodProcessor;
+import java.util.*;
+
+
 
 /*
  * This OpMode illustrates the concept of driving a path based on encoder counts.
@@ -78,6 +85,11 @@ public class RobotAuto extends LinearOpMode {
     public Servo clawPush = null;
     public Servo clawDrop = null;
 
+    private TfodProcessor tfod;
+
+    private VisionPortal visionPortal;
+
+
     private ElapsedTime runtime = new ElapsedTime();
 
     // Calculate the COUNTS_PER_INCH for your specific drive train.
@@ -86,7 +98,7 @@ public class RobotAuto extends LinearOpMode {
     // For example, use a value of 2.0 for a 12-tooth spur gear driving a 24-tooth spur gear.
     // This is gearing DOWN for less speed and more torque.
     // For gearing UP, use a gear ratio less than 1.0. Note this will affect the direction of wheel rotation.
-    static final double     COUNTS_PER_MOTOR_REV    = 537.7*4;    // eg: TETRIX Motor Encoder
+    static final double     COUNTS_PER_MOTOR_REV    = 537.7;    // eg: TETRIX Motor Encoder
     static final double     DRIVE_GEAR_REDUCTION    = 1.0;     // No External Gearing.
     static final double     WHEEL_DIAMETER_INCHES   = 3.77953;     // For figuring circumference
     static final double     COUNTS_PER_INCH         = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) /
@@ -98,10 +110,10 @@ public class RobotAuto extends LinearOpMode {
     public void runOpMode() {
 
         // Initialize the drive system variables.
-        frontLeft  = hardwareMap.get(DcMotor.class, "left_front_drive");
-        frontRight = hardwareMap.get(DcMotor.class, "right_front_drive");
-        rearLeft = hardwareMap.get(DcMotor.class, "left_rear_drive");
-        rearRight = hardwareMap.get(DcMotor.class, "right_rear_drive");
+        frontLeft  = hardwareMap.dcMotor.get("left_front_drive");
+        frontRight = hardwareMap.dcMotor.get("right_front_drive");
+        rearLeft = hardwareMap.dcMotor.get("left_rear_drive");
+        rearRight = hardwareMap.dcMotor.get("right_rear_drive");
         //upperArmJoint = hardwareMap.get(DcMotor.class, "arm_upper_joint");
         //lowerArmJoint = hardwareMap.get(DcMotor.class, "arm_lower_joint");
 
@@ -109,9 +121,9 @@ public class RobotAuto extends LinearOpMode {
         // When run, this OpMode should start both motors driving forward. So adjust these two lines based on your first test drive.
         // Note: The settings here assume direct drive on left and right wheels.  Gear Reduction or 90 Deg drives may require direction flips
         frontLeft.setDirection(DcMotor.Direction.FORWARD);
-        frontRight.setDirection(DcMotor.Direction.REVERSE);
-        rearLeft.setDirection(DcMotor.Direction.FORWARD);
-        rearRight.setDirection(DcMotor.Direction.REVERSE);
+        frontRight.setDirection(DcMotor.Direction.FORWARD);
+        rearLeft.setDirection(DcMotor.Direction.REVERSE);
+        rearRight.setDirection(DcMotor.Direction.FORWARD);
 
         frontLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         frontRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
@@ -124,12 +136,12 @@ public class RobotAuto extends LinearOpMode {
         frontRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         rearLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         rearRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-<<<<<<< Updated upstream
-        upperArmJoint.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-=======
         //upperArmJoint.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
->>>>>>> Stashed changes
         //lowerArmJoint.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        tfod = TfodProcessor.easyCreateWithDefaults();
+        visionPortal = VisionPortal.easyCreateWithDefaults(hardwareMap.get(WebcamName.class, "Webcam 1"), tfod);
+        visionPortal.stopLiveView();
 
         // Send telemetry message to indicate successful Encoder reset
         telemetry.addData("Starting at",  "%7d :%7d",
@@ -147,33 +159,26 @@ public class RobotAuto extends LinearOpMode {
         //encoderDrive(DRIVE_SPEED,  48,  48, 5.0);  // S1: Forward 47 Inches with 5 Sec timeout
         //encoderDrive(TURN_SPEED,   12, -12, 4.0);  // S2: Turn Right 12 Inches with 4 Sec timeout
         //encoderDrive(DRIVE_SPEED, -24, -24, 4.0);  // S3: Reverse 24 Inches with 4 Sec timeout
-<<<<<<< Updated upstream
-        sleep(500);
-        move(30);
-        //sleep(100);
-        turn(Math.PI);
-        //sleep(100);
-        move(30);
-
-=======
-
-        timeDrive(0.05, 12);
-        sleep(5000);
-        timeDrive(0.05, -12);
->>>>>>> Stashed changes
+        //sleep(500);
+        //move(30);
+        /*
+        turn(Math.PI/2);
 
         telemetry.addData("Path", "Complete");
         telemetry.update();
         sleep(1000);  // pause to display final telemetry message.
-    }
+        while(opModeIsActive()){
+            telemetry.addData("pixelInFront: ", Boolean.toString(objectInFront()));
+            telemetry.update();
+        }
+         */
 
-<<<<<<< Updated upstream
+        auto();
+    }
     public void turn(double radians){
         encoderDrive(TURN_SPEED, -(radians * 20.043971751969), (radians * 20.043971751969), 1000);
     }
 
-=======
->>>>>>> Stashed changes
     public void move(double inches){
         encoderDrive(DRIVE_SPEED, inches, inches, 30);
     }
@@ -235,24 +240,6 @@ public class RobotAuto extends LinearOpMode {
     public void dropPixel(){
 
     }
-    public void timeDrive(double speed, double inches){
-        double time = 5.14*inches/(12*speed);
-        if(inches < 0)
-            speed*=-1;
-        frontLeft.setPower(speed);
-        frontRight.setPower(speed);
-        rearRight.setPower(speed);
-        rearLeft.setPower(speed);
-        runtime.reset();
-        while (opModeIsActive() && runtime.seconds() < time){
-            telemetry.addData("time calculated:", time);
-            telemetry.update();
-        }
-        frontRight.setPower(0);
-        frontLeft.setPower(0);
-        rearLeft.setPower(0);
-        rearRight.setPower(0);
-    }
     public void encoderDrive(double speed,
                              double leftInches, double rightInches,
                              double timeoutS) {
@@ -261,7 +248,6 @@ public class RobotAuto extends LinearOpMode {
         int newRearLeftTarget;
         int newRearRightTarget;
 
-        telemetry.addLine("running encoder drive");
         // Ensure that the OpMode is still active
         if (opModeIsActive()) {
 
@@ -289,6 +275,8 @@ public class RobotAuto extends LinearOpMode {
             rearLeft.setPower(Math.abs(speed));
             rearRight.setPower(Math.abs(speed));
 
+            //sleep(10000);
+
             // keep looping while we are still active, and there is time left, and both motors are running.
             // Note: We use (isBusy() && isBusy()) in the loop test, which means that when EITHER motor hits
             // its target position, the motion will stop.  This is "safer" in the event that the robot will
@@ -301,6 +289,12 @@ public class RobotAuto extends LinearOpMode {
 
                 // Display it for the driver.
                 telemetry.addData("Running to",  " %7d :%7d", frontLeft.getTargetPosition(),  newFrontLeftTarget);
+                //telemetry.addData("Runtime: ", runtime.seconds());
+                telemetry.addData("front left power: ", frontLeft.getPower());
+                telemetry.addData("front right power: ", frontRight.getPower());
+                telemetry.addData("rear left power: ", rearLeft.getPower());
+                telemetry.addData("rear right power: ", rearRight.getPower());
+
                 telemetry.addData("Currently at",  " at %7d :%7d",
                                             frontLeft.getCurrentPosition(), frontRight.getCurrentPosition(), rearRight.getCurrentPosition(), rearLeft.getCurrentPosition());
                 telemetry.update();
@@ -321,21 +315,35 @@ public class RobotAuto extends LinearOpMode {
             sleep(250);   // optional pause after each move.
         }
     }
-<<<<<<< Updated upstream
-}
-=======
-
-    //TODO: Test the turn and move functions
-    public void turn(double radians){
-        encoderDrive(TURN_SPEED, -(radians * Math.sqrt(550)) / 2.54, (radians * Math.sqrt(550)) / 2.54, 1000);
-    }
 
     public boolean objectInFront(){
+        //visionPortal.resumeStreaming();
+        List<Recognition> currentRecognitions = tfod.getRecognitions();
+        telemetry.addData("# Objects Detected", currentRecognitions.size());
+
+        // Step through the list of recognitions and display info for each one.
+        for (Recognition recognition : currentRecognitions) {
+            double x = (recognition.getLeft() + recognition.getRight()) / 2 ;
+            double y = (recognition.getTop()  + recognition.getBottom()) / 2 ;
+
+            telemetry.addData(""," ");
+            telemetry.addData("Image", "%s (%.0f %% Conf.)", recognition.getLabel(), recognition.getConfidence() * 100);
+            telemetry.addData("- Position", "%.0f / %.0f", x, y);
+            telemetry.addData("- Size", "%.0f x %.0f", recognition.getWidth(), recognition.getHeight());
+
+
+            if(recognition.getConfidence() > 0.7 && recognition.getLabel().equals("Pixel")) {
+                //visionPortal.stopStreaming();
+                return true;
+            }
+        }
+
+        //visionPortal.stopStreaming();
         return false;
-        //TODO: Finish this
     }
 
-/*    public void placeObject(){
+
+    /*    public void placeObject(){
         //TODO: Finish this
     }
 
@@ -375,4 +383,3 @@ public class RobotAuto extends LinearOpMode {
 
     }
 }
->>>>>>> Stashed changes
