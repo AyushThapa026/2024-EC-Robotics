bc3044eeb2fc83066e00f57a8d29536ee538f243
AyushThapa026 <ayushthapa026@gmail.com>
2023-12-19 14:44:48 -0800

    VisionAuto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotAuto.java
@@ -1,32 +1,3 @@
-/* Copyright (c) 2017 FIRST. All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without modification,
- * are permitted (subject to the limitations in the disclaimer below) provided that
- * the following conditions are met:
- *
- * Redistributions of source code must retain the above copyright notice, this list
- * of conditions and the following disclaimer.
- *
- * Redistributions in binary form must reproduce the above copyright notice, this
- * list of conditions and the following disclaimer in the documentation and/or
- * other materials provided with the distribution.
- *
- * Neither the name of FIRST nor the names of its contributors may be used to endorse or
- * promote products derived from this software without specific prior written permission.
- *
- * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
- * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
- * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
-
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
@@ -40,64 +11,15 @@ import org.firstinspires.ftc.vision.VisionPortal;
 import org.firstinspires.ftc.vision.tfod.TfodProcessor;
 import java.util.*;
 
-
-
-/*
- * This OpMode illustrates the concept of driving a path based on encoder counts.
- * The code is structured as a LinearOpMode
- *
- * The code REQUIRES that you DO have encoders on the wheels,
- *   otherwise you would use: RobotAutoDriveByTime;
- *
- *  This code ALSO requires that the drive Motors have been configured such that a positive
- *  power command moves them forward, and causes the encoders to count UP.
- *
- *   The desired path in this example is:
- *   - Drive forward for 48 inches
- *   - Spin right for 12 Inches
- *   - Drive Backward for 24 inches
- *   - Stop and close the claw.
- *
- *  The code is written using a method called: encoderDrive(speed, leftInches, rightInches, timeoutS)
- *  that performs the actual movement.
- *  This method assumes that each movement is relative to the last stopping place.
- *  There are other ways to perform encoder based moves, but this method is probably the simplest.
- *  This code uses the RUN_TO_POSITION mode to enable the Motor controllers to generate the run profile
- *
- * Use Android Studio to Copy this Class, and Paste it into your team's code folder with a new name.
- * Remove or comment out the @Disabled line to add this OpMode to the Driver Station OpMode list
- */
-
 @Autonomous(name="Auto", group="Robot")
 public class RobotAuto extends LinearOpMode {
-
-    /* Declare OpMode members. */
-
     public DcMotor  frontLeft   = null;
     public DcMotor  frontRight  = null;
     public DcMotor  rearLeft    = null;
     public DcMotor  rearRight   = null;
-    public DcMotor leftLinearSlide = null;
-    public DcMotor rightLinearSlide = null;
-    public DcMotor upperArmJoint = null;
-    public DcMotor lowerArmJoint = null;
-    public DcMotor rootArmJoint = null;
     public Servo clawPush = null;
     public Servo clawDrop = null;
-
-    private TfodProcessor tfod;
-
-    private VisionPortal visionPortal;
-
-
     private ElapsedTime runtime = new ElapsedTime();
-
-    // Calculate the COUNTS_PER_INCH for your specific drive train.
-    // Go to your motor vendor website to determine your motor's COUNTS_PER_MOTOR_REV
-    // For external drive gearing, set DRIVE_GEAR_REDUCTION as needed.
-    // For example, use a value of 2.0 for a 12-tooth spur gear driving a 24-tooth spur gear.
-    // This is gearing DOWN for less speed and more torque.
-    // For gearing UP, use a gear ratio less than 1.0. Note this will affect the direction of wheel rotation.
     static final double     COUNTS_PER_MOTOR_REV    = 537.7;    // eg: TETRIX Motor Encoder
     static final double     DRIVE_GEAR_REDUCTION    = 1.0;     // No External Gearing.
     static final double     WHEEL_DIAMETER_INCHES   = 3.77953;     // For figuring circumference
@@ -105,22 +27,16 @@ public class RobotAuto extends LinearOpMode {
                                                       (WHEEL_DIAMETER_INCHES * 3.1415);
     static final double     DRIVE_SPEED             = 0.3;
     static final double     TURN_SPEED              = 0.3;
-
+    public VisionAuto VisionClassAuto;
     @Override
     public void runOpMode() {
-
         // Initialize the drive system variables.
         frontLeft  = hardwareMap.dcMotor.get("left_front_drive");
         frontRight = hardwareMap.dcMotor.get("right_front_drive");
         rearLeft = hardwareMap.dcMotor.get("left_rear_drive");
         rearRight = hardwareMap.dcMotor.get("right_rear_drive");
-        //upperArmJoint = hardwareMap.get(DcMotor.class, "arm_upper_joint");
-        //lowerArmJoint = hardwareMap.get(DcMotor.class, "arm_lower_joint");
         clawPush = hardwareMap.servo.get("pixel_push");
 
-        // To drive forward, most robots need the motor on one side to be reversed, because the axles point in opposite directions.
-        // When run, this OpMode should start both motors driving forward. So adjust these two lines based on your first test drive.
-        // Note: The settings here assume direct drive on left and right wheels.  Gear Reduction or 90 Deg drives may require direction flips
         frontLeft.setDirection(DcMotor.Direction.FORWARD);
         frontRight.setDirection(DcMotor.Direction.FORWARD);
         rearLeft.setDirection(DcMotor.Direction.REVERSE);
@@ -130,12 +46,6 @@ public class RobotAuto extends LinearOpMode {
         frontRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         rearLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         rearRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        //lowerArmJoint.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-
-        tfod = TfodProcessor.easyCreateWithDefaults();
-        visionPortal = VisionPortal.easyCreateWithDefaults(hardwareMap.get(WebcamName.class, "Webcam 1"), tfod);
-        visionPortal.setProcessorEnabled(tfod, false);
-        visionPortal.stopLiveView(); // Save CPU resources
 
         // Send telemetry message to indicate successful Encoder reset
         telemetry.addData("Starting at",  "%7d :%7d",
@@ -147,14 +57,13 @@ public class RobotAuto extends LinearOpMode {
 
         // Wait for the game to start (driver presses PLAY)
         waitForStart();
-
-        auto();
     }
-    public void turn(double radians){
+
+    private void turn(double radians){
         encoderDrive(TURN_SPEED, -(radians * 20.043971751969), (radians * 20.043971751969), 1000);
     }
 
-    public void move(double inches){
+    private void move(double inches){
         encoderDrive(DRIVE_SPEED, inches, inches, 30);
     }
 
@@ -162,24 +71,13 @@ public class RobotAuto extends LinearOpMode {
         return (COUNTS_PER_MOTOR_REV / (2 * Math.PI)) * r;
     }
 
-    /*
-     *  Method to perform a relative move, based on encoder counts.
-     *  Encoders are not reset as the move is based on the current position.
-     *  Move will stop if any of three conditions occur:
-     *  1) Move gets to the desired position
-     *  2) Move runs out of time
-     *  3) Driver stops the OpMode running.
-     */
     public void pushPixel(){
         move(4);
         clawPush.setPosition(0); // TODO: Find the actual position
         move(-4);
     }
 
-    public void dropPixel(){
-
-    }
-    public void encoderDrive(double speed,
+    private void encoderDrive(double speed,
                              double leftInches, double rightInches,
                              double timeoutS) {
         int newFrontLeftTarget;
@@ -214,14 +112,6 @@ public class RobotAuto extends LinearOpMode {
             rearLeft.setPower(Math.abs(speed));
             rearRight.setPower(Math.abs(speed));
 
-            //sleep(10000);
-
-            // keep looping while we are still active, and there is time left, and both motors are running.
-            // Note: We use (isBusy() && isBusy()) in the loop test, which means that when EITHER motor hits
-            // its target position, the motion will stop.  This is "safer" in the event that the robot will
-            // always end the motion as soon as possible.
-            // However, if you require that BOTH motors have finished their moves before the robot continues
-            // onto the next step, use (isBusy() || isBusy()) in the loop test.
             while (opModeIsActive() &&
                    (runtime.seconds() < timeoutS) &&
                    (frontLeft.isBusy() && frontRight.isBusy() && rearRight.isBusy() && rearLeft.isBusy())) {
@@ -249,104 +139,5 @@ public class RobotAuto extends LinearOpMode {
 
             sleep(250);   // optional pause after each move.
         }
-    }
-
-    public boolean objectInFront(){
-        //visionPortal.resumeStreaming();
-        visionPortal.setProcessorEnabled(tfod, true);
-        List<Recognition> currentRecognitions = tfod.getRecognitions();
-        telemetry.addData("# Objects Detected", currentRecognitions.size());
-
-        // Step through the list of recognitions and display info for each one.
-        for (Recognition recognition : currentRecognitions) {
-            double x = (recognition.getLeft() + recognition.getRight()) / 2 ;
-            double y = (recognition.getTop()  + recognition.getBottom()) / 2 ;
-
-            telemetry.addData(""," ");
-            telemetry.addData("Image", "%s (%.0f %% Conf.)", recognition.getLabel(), recognition.getConfidence() * 100);
-            telemetry.addData("- Position", "%.0f / %.0f", x, y);
-            telemetry.addData("- Size", "%.0f x %.0f", recognition.getWidth(), recognition.getHeight());
-
-
-            if(recognition.getConfidence() > 0.7 && recognition.getLabel().equals("Pixel")) {
-                //visionPortal.stopStreaming();
-                visionPortal.setProcessorEnabled(tfod, false);
-                return true;
-            }
-        }
-
-        visionPortal.setProcessorEnabled(tfod, false);
-        //visionPortal.stopStreaming();
-        return false;
-    }
-
-    public void auto(){
-        int pos;
-        /*
-        move(18);
-        if (objectInFront()){
-            pos = 1;
-            pushPixel();
-            turn(-Math.PI/2);
-            move(23);
-        } else {
-            turn(-Math.PI/2);
-            move(22);
-            turn(Math.PI/2);
-            if(objectInFront()){
-                pos = 2;
-                pushPixel();
-                turn(-Math.PI/2);
-            } else {
-                move(13);
-                turn(Math.PI/2);
-                move(6);
-                pos = 3;
-                pushPixel();
-                turn(-Math.PI/2);
-                move(-13);
-                turn(-Math.PI/2);
-                move(6);
-            }
-
-        }
-         */
-        move(6);
-        turn(-Math.PI/2); // right
-        move(18);
-        turn(Math.PI/2); // left
-        move(30);
-        telemetry.addData("Pixel detected:", objectInFront());
-        telemetry.update();
-        /*
-        if(objectInFront()){
-            pushPixel();
-            move(-24);
-            turn(-Math.PI/2);
-            move(46);
-        } else {
-            move(12+9);
-            turn(Math.PI/2);
-            move(5);
-            if(objectInFront()){
-                pushPixel();
-                move(-5);
-                turn(Math.PI/2);
-                move(42);
-                turn(Math.PI/2);
-                move(42);
-            }else {
-                turn(Math.PI/2);
-                move(6);
-                turn(-Math.PI/2);
-                move(13);
-                pushPixel();
-                turn(Math.PI/2);
-                move(36);
-                turn(Math.PI/2);
-                move(48);
-            }
-        }
-         */
     }
 }
